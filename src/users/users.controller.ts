b27ed078ec482eb7e59e609ddd16d419baa7c0b1
Aleksey Yuriev <alex11-90@yandex.by>
2024-11-20 10:32:24 +0100

    Feature: add users dtos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,34 +5,34 @@ import {
   Param,
   Query,
   Body,
-  Headers,
-  Ip,
   ParseIntPipe,
   DefaultValuePipe,
+  Patch,
 } from '@nestjs/common'
+import { CreateUserDto } from './dtos/create-user.dto'
+import { GetUsersParamDto } from './dtos/get-users-param.dto'
+import { PatchUserDto } from './dtos/patch-user.dto'
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number
   ) {
-    console.log(limit)
-    console.log(page)
+    console.log(getUsersParamDto)
     return 'You sent a get request to users endpoint'
   }
 
   @Post()
-  public createUsers(
-    @Body() request: any,
-    @Headers() headers: any,
-    @Ip() ip: any
-  ) {
-    console.log(request)
-    console.log(headers)
-    console.log(ip)
+  public createUsers(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto instanceof CreateUserDto)
     return 'You sent a post request to users endpoint'
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto
   }
 }
