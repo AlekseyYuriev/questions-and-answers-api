072825c87285239e400455bc040e0ea110faefc8
Aleksey Yuriev <alex11-90@yandex.by>
2024-11-20 16:20:48 +0100

    Feature: add CreateQuestionDto and CreateAnswerDto annotation for swagger

--- src/answers/dtos/create-answer.dto.ts
+++ src/answers/dtos/create-answer.dto.ts
@@ -1,31 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger'
 import {
   IsInt,
   IsISO8601,
   IsNotEmpty,
-  IsOptional,
   IsString,
-  Max,
   Min,
   MinLength,
 } from 'class-validator'
 
 export class CreateAnswerDto {
+  @ApiProperty({
+    description: 'This is the text of the answer',
+    example: 'Try to change the connection port',
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   text: string
 
+  @ApiProperty({
+    description: "That's the name of the author of the answer",
+    example: 'Alice',
+  })
   @IsString()
   @IsNotEmpty()
   author: string
 
+  @ApiProperty({
+    description: 'This is rating for the answer',
+    example: 7,
+  })
   @IsInt()
   @Min(0)
   rating: number
 
+  @ApiProperty({
+    description: 'The date on which the answer is published',
+    example: '2024-11-20T14:20:41.117Z',
+  })
   @IsISO8601()
   createdAt: Date
 
+  @ApiProperty({
+    description: 'The date on which the answer is updated',
+    example: '2024-11-20T14:20:41.117Z',
+  })
   @IsISO8601()
   updatedAt: Date
 }
--- src/questions/dtos/create-question.dto.ts
+++ src/questions/dtos/create-question.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import {
   IsArray,
   IsInt,
@@ -5,39 +6,67 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
-  Max,
   Min,
   MinLength,
 } from 'class-validator'
 
 export class CreateQuestionDto {
+  @ApiProperty({
+    description: 'This is rating for the question',
+    example: 3,
+  })
   @IsInt()
   @Min(0)
   rating: number
 
+  @ApiProperty({
+    description: "That's the name of the author of the question",
+    example: 'John',
+  })
   @IsString()
   @IsNotEmpty()
   author: string
 
+  @ApiProperty({
+    description: "That's the title of the question",
+    example: 'How to dockerize Redis in NestJS app?',
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string
 
+  @ApiProperty({
+    description: "That's the description of the question",
+    example:
+      "I've addes redis service to docker-compose file, but the app is crashing when I try to up the docker",
+  })
   @IsString()
   @MinLength(8)
   @IsNotEmpty()
   description: string
 
+  @ApiPropertyOptional({
+    description: 'Array of tags passed as string values',
+    example: ['nestjs', 'docker', 'redis'],
+  })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags?: string[]
 
+  @ApiProperty({
+    description: 'The date on which the question is published',
+    example: '2024-11-20T14:20:41.117Z',
+  })
   @IsISO8601()
   createdAt: Date
 
+  @ApiProperty({
+    description: 'The date on which the question is updated',
+    example: '2024-11-20T14:20:41.117Z',
+  })
   @IsISO8601()
   updatedAt: Date
 }
