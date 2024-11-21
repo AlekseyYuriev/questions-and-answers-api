f0e758d9e7165525a8f9cd13303bb097b74500f0
Aleksey Yuriev <alex11-90@yandex.by>
2024-11-21 11:17:54 +0100

    Feature: set up compodoc for code documentation

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common'
 import { GetUsersParamDto } from '../dtos/get-users-param.dto'
 
+/**
+ * Class to connect to Users table and perform business operations
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * The method to get all the users from the database
+   */
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number,
@@ -20,6 +26,9 @@ export class UsersService {
     ]
   }
 
+  /**
+   * Find a single user using the ID of the user
+   */
   public findOneById(id: number) {
     return {
       id: 1234,
