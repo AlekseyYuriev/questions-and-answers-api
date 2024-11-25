6e48df9decf0be0cb0def60bc5a03d1bf05f8930
Aleksey Yuriev <alex11-90@yandex.by>
2024-11-25 11:29:37 +0100

    Feature: add tagsService

---
+++ src/tags/providers/tags.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTagDto } from '../dtos/create-tag.dto';
+import { Repository } from 'typeorm';
+import { Tag } from '../tag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class TagsService {
+  constructor(
+    /**
+     * Inject tagsRepository
+     */
+    @InjectRepository(Tag)
+    private readonly tagsRepository: Repository<Tag>
+  ) {}
+
+  public async createTag(createTagDto: CreateTagDto) {
+    let tag = this.tagsRepository.create(createTagDto);
+
+    return await this.tagsRepository.save(tag);
+  }
+}
--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreateTagDto } from './dtos/create-tag.dto';
+import { TagsService } from './providers/tags.service';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(
+    /**
+     * Inject TagsService
+     */
+    private readonly TagsService: TagsService
+  ) {}
+
+  @ApiOperation({
+    summary: 'Creates a new tag',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if your tag is created successfully',
+  })
+  @Post()
+  public createTag(@Body() createTagDto: CreateTagDto) {
+    return this.TagsService.createTag(createTagDto);
+  }
+}
