08ce2aab2a6956b893f7774591f58c0d3cfa5f42
Aleksey Yuriev <alex11-90@yandex.by>
2024-11-26 21:46:47 +0300

    Feature: add migrations to add Answers, Tags and tagsToQuestions

---
+++ src/migrations/1732645915173-addTagsToQuestions.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddTagsToQuestions1732645915173 implements MigrationInterface {
+  name = 'AddTagsToQuestions1732645915173';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        INSERT INTO "question_tags_tag" ("questionId", "tagId") VALUES (1, 1);
+        INSERT INTO "question_tags_tag" ("questionId", "tagId") VALUES (1, 2);
+        INSERT INTO "question_tags_tag" ("questionId", "tagId") VALUES (2, 2);
+        INSERT INTO "question_tags_tag" ("questionId", "tagId") VALUES (2, 3);
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        DELETE FROM "question_tags_tag" WHERE "questionId" = 1 AND "tagId" = 1;
+        DELETE FROM "question_tags_tag" WHERE "questionId" = 1 AND "tagId" = 2;
+        DELETE FROM "question_tags_tag" WHERE "questionId" = 2 AND "tagId" = 2;
+        DELETE FROM "question_tags_tag" WHERE "questionId" = 2 AND "tagId" = 3;
+    `);
+  }
+}
