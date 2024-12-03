a41da51cec30773304edb91e074894cdd66d43ac
Aleksey Yuriev <alex11-90@yandex.by>
2024-12-03 15:00:20 +0100

    Refactor: put redis configuration to separate config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisModule } from '@nestjs-modules/ioredis';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { QuestionsModule } from './questions/questions.module';
 import { AnswersModule } from './answers/answers.module';
 import { TagsModule } from './tags/tags.module';
 import { RolesModule } from './roles/roles.module';
 import { TypeOrmConfigService } from './config/typeOrm/typeOrmConfigService';
+import { RedisConfigService } from './config/redis/redisConfigService';
 
 @Module({
   imports: [
@@ -21,12 +22,7 @@ import { TypeOrmConfigService } from './config/typeOrm/typeOrmConfigService';
       useClass: TypeOrmConfigService,
     }),
     RedisModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'single',
-        url: configService.get('CACHE_URL'),
-      }),
+      useClass: RedisConfigService,
     }),
     UsersModule,
     QuestionsModule,
